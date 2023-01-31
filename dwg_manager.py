a76e00604bff004b2d7235f6827d4a5e7cbc6d69
Andre <andreftardin@gmail.com>
2023-01-31 11:58:59 -0500

    Classes for RFIs and Drawings

--- dwg_manager.py
+++ dwg_manager.py
@@ -1,19 +1,33 @@
 
+drawings = {"Strucutral":0, "Architectural":0, "Mechanical":0}
+
 class Drawings:
-    def __init__(self, subject, revision, has_rfis=False):
+    def __init__(self, subject, revision, total_rfis=0):
         self.subject = subject
         self.revision = revision
-        self.has_rfis = has_rfis
+        self.total_rfis = total_rfis
     
     def __repr__(self):
-        if self.has_rfis == True:
-            return "The {subject} drawing is currently on revision {revision} and has RFIs.".format(subject=self.subject, revision=self.revision)
+        if self.total_rfis > 0:
+            return "The {subject} drawing is currently on revision {revision} and has {total_rfis} RFIs.".format(subject=self.subject, revision=self.revision, total_rfis=self.total_rfis)
         else:
-            return "The {subject} drawing is currently on revision {revision} and has no RFI.".format(subject=self.subject, revision=self.revision)
+            return "The {subject} drawing is currently on revision {revision} and has no RFIs.".format(subject=self.subject, revision=self.revision)
 
-
-drawings = {}
 rfis = {}
+
+class RFI:
+    def __init__(self, subject, question, number=len(rfis)+1, resolved=False, answer=""):
+        self.subject = subject
+        self.number = number
+        self.question = question
+        self.resolved = resolved
+        self.answer = answer
+    
+    def __repr__(self):
+        if self.resolved:
+            return "The {subject} drawing has RFI {number} resolved. Question below:\n{question}\nAnswer:\n{answer}".format(subject=self.subject, number=self.number, question=self.question, answer=self.answer)
+        else:
+            return "The {subject} drawing has RFI {number} not resolved. Question below:\n {question}".format(subject=self.subject, number=self.number, question=self.question)
 
 
 def drawing_menu():
@@ -21,10 +35,9 @@ def drawing_menu():
 def rfi_menu_selection():
     rfi_menu_selection = input("Select below:\n 1. Add new RFI;\n 2. View Outstanding RFIs;\n 3. View Completed RFIs. \n")
 
-welcome_message = "Welcome to Drawing Manager!"
-options_message = "For Drawings press 1.\nFor RFIs press 2."
+welcome_message = "Welcome to Drawing Manager!\n For Drawings press 1.\n For RFIs press 2."
 
-user_selection = input(welcome_message + "\n" + options_message + "\nTo exit press 3.\n")
+user_selection = input(welcome_message + "\n To exit press 3.\n")
 
 if user_selection == "1":
     drawing_menu()
