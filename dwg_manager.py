44c817d930331fa41ca0a0ecf0fb0efc0d164045
Andre <andreftardin@gmail.com>
2023-01-31 12:25:19 -0500

    Redone main menu selection to work with wrong inputs

--- dwg_manager.py
+++ dwg_manager.py
@@ -1,6 +1,9 @@
 
+#Define Dictionaries for DWG and RFI
 drawings = {"Strucutral":0, "Architectural":0, "Mechanical":0}
+rfis = {}
 
+#Create Classes
 class Drawings:
     def __init__(self, subject, revision, total_rfis=0):
         self.subject = subject
@@ -12,8 +15,6 @@ class Drawings:
             return "The {subject} drawing is currently on revision {revision} and has {total_rfis} RFIs.".format(subject=self.subject, revision=self.revision, total_rfis=self.total_rfis)
         else:
             return "The {subject} drawing is currently on revision {revision} and has no RFIs.".format(subject=self.subject, revision=self.revision)
-
-rfis = {}
 
 class RFI:
     def __init__(self, subject, question, number=len(rfis)+1, resolved=False, answer=""):
@@ -29,23 +30,22 @@ class RFI:
         else:
             return "The {subject} drawing has RFI {number} not resolved. Question below:\n {question}".format(subject=self.subject, number=self.number, question=self.question)
 
-
+#Define Functions
 def drawing_menu():
     drawings_menu_selection = input("Select subject:\n 1. Structural;\n 2. Architectural;\n 3. Mechanical.\n")
 def rfi_menu_selection():
     rfi_menu_selection = input("Select below:\n 1. Add new RFI;\n 2. View Outstanding RFIs;\n 3. View Completed RFIs. \n")
 
-welcome_message = "Welcome to Drawing Manager!\n For Drawings press 1.\n For RFIs press 2."
+welcome_message = "Welcome to Drawing Manager!"
+options_message = "\n For Drawings press 1.\n For RFIs press 2.\n To exit press 3.\n"
+user_selection = input(welcome_message + options_message)
 
-user_selection = input(welcome_message + "\n To exit press 3.\n")
+while user_selection != "1" and user_selection != "2" and user_selection != "3":
+    user_selection = input("Wrong selection, Try again." + options_message)
 
 if user_selection == "1":
     drawing_menu()
 elif user_selection == "2":
     rfi_menu_selection() 
 else:
-    while user_selection != "1" and user_selection != "2" and user_selection != "3":
-        print("Wrong selection, Try again.\n" + options_message + "\nFor exit press 3.")
-        user_selection = input()
-
-
+    print("Exiting Drawing Manager!")
